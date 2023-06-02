ada14000aaa06846db5f7404673dc7ce4e410c15
Mark Penovich <mpenovich1@gmail.com>
2023-06-01 22:23:30 -0500

    1.2

--- basic-operations1.1.scm
+++ basic-operations1.1.scm
@@ -1,3 +1,5 @@
+; ch 1
+; 1.1 
 (display (+ 137 349))
 (newline)
 (display (- 1000 334))
@@ -13,4 +15,25 @@
 (display (* 25 4 12))
 (newline)
 (display (+ (* 3 5) (- 10 6)))
+(newline)
+(display (+ (* 3
+	       (+ (* 2 4)
+		  (+ 3 5)))
+	    (+ (- 10 7)
+	       6)))
+(newline)
+
+; 1.2
+(define size 2)
+(* 5 size)
+
+(define pi 3.14159)
+(define radius 10)
+(display (* pi (* radius radius)))
+(newline)
+
+(define circumference (* 2 pi radius))
+(display "circumference")
+(newline)
+(display circumference)
 (newline)
